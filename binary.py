binary class updates, center of mass calculation fixes

binary.py
@@ -6,14 +6,18 @@ that is readable by pynbody.  Just need to pass a string to let it know which in
 
 Rest of docstring: todo
 """
+import numpy as np
 
 #Import my binary star module that continue relevant routines
 import AddBinary
 
-class Binary:
+class Binary(object):
 	"""
 	Defines the binary star class Binary.	
 	Binary star class used for storing positions/velocities and Keplerian Orbital elements for the system.
+	
+	Input Units: Lengths = AU, Velocities = km/s (*29.8 == VEL_UNIT), angles in degrees	
+	Output Units: Same as input	
 	
 	Initializing:
 	
@@ -29,15 +33,93 @@ class Binary:
 	
 	"""	
 	
-	def __init__(self,X,flag):
-		if flag == "Cartesian" or "cartesian":
-			pass
-		elif flag == "Kepler" or "kepler":
-			pass
-		elif flag == "Snapshot" or "snapshot":
+	def __init__(self,X,m1,m2,state):
+		#Ensure input is proper
+		assert (len(X) == 6), "len(Input Array) != 6. len = %d." % len(X)		
+		
+		if state == "Cartesian" or "cartesian":
+			self.r = X[0:3]
+			self.v = X[3:]
+			self.m1 = m1
+			self.m2 = m2
+			self.state = state
+		elif state == "Kepler" or "kepler":
+			self.assignOrbElems(self,X,m1,m2,state)
+			self.m1 = m1
+			self.m2 = m2
+			self.state = state
+		elif state == "Snapshot" or "snapshot":
 			pass
 		else:
-			print "Invalid input data type flag: %s.  All params set to 0 by default." % flag
+			print "Invalid input data type state: %s.  All params set to 0 by default." % state
+		
+	#Member Functions		
+		
+	def assignOrbElems(self,X):
+		"""
+		Given array of orbital elements, set them as class parameters.  Intended as a shorthand function.
+		
+		Input:
+		X is e, a, i, Omega, w, nu which are the eccentricity, semimajor axis (AU),
+		inclination (degrees), Longitude of Ascending Node (degrees), argument of periapsis (degrees), and
+		the true anomaly (degrees).
 			
-			
-	
+		Output:
+		None
+		"""
+		self.e = X[0]
+		self.a = X[1]
+		self.i = X[2]
+		self.Omega = X[3]
+		self.w = X[4]
+		self.nu = X[5]
+		return
+
+	def computeOrbElems(self):
+		"""
+		Compute the Kepler orbital elements.
+		
+		Input: Self (must have r, v set and in proper units)
+		
+		Output: sets and returns e,a,i,... 
+		"""
+		#Compute orbital elements from binary center of mass frame Cartesian coordinates
+		zero = np.asarray([0,0,0])
+		oe = AddBinary.calcOrbitalElements(self.r,zero,self.v,zero,self.m1,self.m2)
+
+		#Set orbital elements, return them as well		
+		self.assignOrbElems(oe)
+		return oe
+		
+	def computeCartesian(self):
+		"""
+		Compute the Cartesian position and velocity in the reduced mass frame.
+		"""
+		assert (self.state == "Kepler" or "kepler")
+		zero = np.asarray([0.,0.,0.])		
+		M = AddBinary.calcMeanAnomaly(self.r,zero,self.v,zero,self.m1,self.m2)
+		self.r, self.v = AddBinary.keplerToCartesian(self.a,self.e,self.i,self.Omega,self.w,M,self.m1,self.m2)
+		
+	def generateICs(self):
+		"""
+		From Kepler orbital elements, compute the initial position, velocities for two stars in ChaNGa-friendly units.
+		"""
+		zero = np.asarray([0.,0.,0.])
+		if self.state == "Kepler" or "kepler":
+			M = AddBinary.calcMeanAnomaly(self.r,zero,self.v,zero,self.m1,self.m2)
+			x1,x2,v1,v2 = AddBinary.initializeBinary(self.a,self.e,self.i,self.Omega,self.w,M,self.m1,self.m2)
+		elif self.state == "Cartesian" or "cartesian":
+			oe = self.computeOrbElems(self.r,zero,self.v,zero,self.m1,self.m2)
+			x1,x2,v1,v2 = AddBinary.initializeBinary(self.a,self.e,self.i,self.Omega,self.w,M,self.m1,self.m2)
+		else:
+			pass
+		
+		return x1, x2, v1, v2
+		
+		
+		
+		
+		
+		
+		
+		

binaryUtils.py
@@ -197,28 +197,39 @@ def find_crit_radius(r,array,toFind,num=1000):
 
 #end function
 
-def computeSystemCOM(stars,gas):
+def computeCOM(stars,gas,cutoff=None,starFlag=True):
 	"""
-	Given pynbody star and gas arrays, compute the center of mass for the entire system.
+	Given pynbody star and gas arrays, compute the center of mass for the entire specified system.
 
 	Inputs: (pynbody objects!)
 	stars: s.stars pynbody object
 	gas: s.gas pynbody object
+	cutoff: radius at which you only consider objects interior to it
+	starFlag: whether or not to consider stars
 
 	Output:
 	Center of mass (in AU for each x,y,z component) as numpy array
-	
-	Added support to work on my laptop (No idea why it wouldn't before?) 3/11/2015 dflemin3
+
+	Note: a lot of "strip_units" commands included to prevent throwing weird value errors.  As long as all masses
+	are in solar masses and positions in AU before this is run, you won't have any problems.	
 	"""
-	#Ensure system in binary
-	assert len(stars) == 2
+	#If there's a cutoff, select gas particles with cylindrical radius less than the cutoff
+	if cutoff != None:
+		mask = gas['rxy'] < cutoff
+		gas = gas[mask]
 	
-	#Compute stellar mass, mass-weighted position
-	starMass = np.sum(stars['mass'])
-	starPos = (stars[0]['pos']*isaac.strip_units(stars[0]['mass']) + stars[1]['pos']*isaac.strip_units(stars[1]['mass']))
+	if starFlag: #Include stars
+		#Ensure binary
+		assert len(stars) == 2
 	
-	#Compute, return total center of mass
-	return np.asarray((starPos + np.sum(gas['pos']*isaac.strip_units(np.mean(gas['mass']))))/np.sum(starMass+np.sum(gas['mass'])))
+		#Compute stellar mass, mass-weighted position
+		starMass = np.sum(stars['mass'])
+		starPos = (stars[0]['pos']*isaac.strip_units(stars[0]['mass']) + stars[1]['pos']*isaac.strip_units(stars[1]['mass']))
+	
+		#Compute, return total center of mass
+		return np.asarray((starPos + np.sum(gas['pos']*isaac.strip_units(np.mean(gas['mass']))))/np.sum(starMass+np.sum(gas['mass'])))
+	else: #No stars, just gas
+		return np.sum(gas['pos']*isaac.strip_units(np.mean(gas['mass'])))/np.sum(gas['mass'])
 
 #end function
 
@@ -286,7 +297,7 @@ def calcNetTorque(stars,gas):
 	assert len(stars) == 2
 
 	#Compute center of mass of entire binary-disk system
-	com = computeSystemCOM(stars,gas)
+	com = computeCOM(stars,gas)
 
 	#Compute net force on primary star (index = 0)
 
@@ -568,7 +579,7 @@ def calcEccVsRadius(s,r,rBinEdges):
 
 #end function
 
-def calcCoMVsRadius(s,r,rBinEdges,starFlag=False):
+def calcCoMVsRadius(s,rBinEdges,starFlag=False):
 	"""
 	Calculates the system's center of mass as a function of radius.  At a given radius r, use the total enclosed
 	mass of the star(s) and gas to compute the center of mass (CoM).  Ideally, I'd like to see the CoM be at
@@ -576,8 +587,7 @@ def calcCoMVsRadius(s,r,rBinEdges,starFlag=False):
 	
 	Inputs: 
 	s: Tipsy-format snapshot readable by pynbody
-	r: numpy array of radial points to calculate on (in xy plane)
-	rBinEdges: edges of the array of radii    
+	rBinEdges: edges of the array of radii in xy plane
 	starFlag: bool for whether or not to consider stars in center of mass calculation
 
 	Output:
@@ -585,21 +595,15 @@ def calcCoMVsRadius(s,r,rBinEdges,starFlag=False):
 	"""
 	stars = s.stars
 	gas = s.gas
-	com = np.zeros((len(r),3))
-	
-	#Make sure I don't screw up lengths and get an off by one error
-	assert(len(r) == len(rBinEdges)-1)	
+	com = np.zeros((len(rBinEdges)-1,3))
 	
 	if starFlag: #Include stars in center of mass calculation
 		#Loop through radial points, select gas within that r, calc CoM
 		for i in range(0,len(rBinEdges)-1):
-			rMask = s.gas['rxy'] < rBinEdges[i]    
-			com[i,:] = computeSystemCOM(stars,gas[rMask])
+			com[i,:] = computeCOM(stars,gas,cutoff=rBinEdges[i],starFlag=starFlag)
 	else: #Gas disk only
 		for i in range(0,len(rBinEdges)-1):
-			rMask = s.gas['rxy'] < rBinEdges[i]    
-			com[i,:] = np.sum(gas['pos'][rMask]*isaac.strip_units(np.mean(gas['mass'][rMask])))/np.sum(gas['mass'][rMask])
-		
+			com[i,:] = computeCOM(stars,gas,cutoff=rBinEdges[i],starFlag=starFlag)
 	return com
 
 #end function