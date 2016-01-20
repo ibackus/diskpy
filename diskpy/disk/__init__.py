f9312041466ee810ea477d3076c810fefa53d5d3
Isaac Backus <ibackus@gmail.com>
2016-01-19 16:03:32 -0800

    Included pSpiralpower_t in diskpy.disk

--- diskpy/disk/__init__.py
+++ diskpy/disk/__init__.py
@@ -11,8 +11,8 @@ from _utils import centerdisk, snapshot_defaults
 from _profiles import Q, Qeff, kappa, height, sigma
 from _math import rho0_est, h_est
 from _spirals import powerspectrum, powerspectrum_t, sigmafft, \
-sigmacylindrical, spiralpower, spiralpower_t
+sigmacylindrical, spiralpower, spiralpower_t, pSpiralpower_t
 
 __all__ = ['centerdisk', 'snapshot_defaults', 'Q', 'Qeff', 'kappa', 'height', 
 'sigma', 'rho0_est', 'h_est', 'powerspectrum', 'powerspectrum_t', 'sigmafft', 
-'sigmacylindrical', 'spiralpower', 'spiralpower_t']
+'sigmacylindrical', 'spiralpower', 'spiralpower_t', 'pSpiralpower_t']
