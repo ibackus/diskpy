90abff1b1c1d4127421a5e975c503c06aee2c98c
Isaac Backus <ibackus@gmail.com>
2016-03-24 12:45:24 -0700

    Added __all__ to pychanga __init__.py to allow auto documentation

--- diskpy/pychanga/__init__.py
+++ diskpy/pychanga/__init__.py
@@ -9,3 +9,7 @@ setup_units
 from _changaOutput import load_acc, walltime, get_fnames, snapshot_time, \
 read_rung_dist
 import hyak
+
+__all__ = ['make_director', 'make_param', 'units_from_param', 'setup_param',
+           'setup_units', 'load_acc', 'walltime', 'get_fnames', 'snapshot_time',
+           'read_rung_dist', 'hyak']
