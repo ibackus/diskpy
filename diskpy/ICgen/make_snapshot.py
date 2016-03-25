8cf7005e8791667ee7f42248d0de4556a969b497
Isaac Backus <ibackus@gmail.com>
2016-03-24 18:11:03 -0700

    Refactored make_snapshot to use several different functions rather than one big function

--- diskpy/ICgen/make_snapshot.py
+++ diskpy/ICgen/make_snapshot.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 """
+Contains functions for genrerating a simulation snapshot from an IC object 
+which already has particle positions generated.  Snapshot are made with
+snapshot_gen(IC)
+
 Created on Fri Mar 21 15:11:31 2014
 
 @author: ibackus
@@ -10,101 +14,150 @@ __version__ = "$Revision: 2 $"
 
 import pynbody
 SimArray = pynbody.array.SimArray
-import numpy as np
 import gc
 
 # diskpy packages
+import diskpy
 from diskpy import global_settings
 from diskpy.utils import match_units, strip_units
-from diskpy.pychanga import make_director, make_param
+from diskpy.pychanga import make_director, make_param, setup_units
 import calc_velocity
 
-def snapshot_gen(ICobj):
+# Constants
+G = SimArray(1.0,'G')
+
+def snapshot_gen(IC):
     """
-    Generates a tipsy snapshot from the initial conditions object ICobj.
+    Generates a tipsy snapshot from the initial conditions object IC.  Includes
+    a routine to calculate the velocity.
     
-    Returns snapshot, param
+    Parameters
+    ----------
+    IC : ICobj
     
-        snapshot: tipsy snapshot
-        param: dictionary containing info for a .param file
+    Returns
+    -------
+    snapshot : SimSnap
+        Simulation snapshot with the velocity calculated
+    param : dict
+        dictionary containing info for a .param file
     """
     
-    print 'Generating snapshot...'
-    # Constants
-    G = SimArray(1.0,'G')
-    # ------------------------------------
-    # Load in things from ICobj
-    # ------------------------------------
-    print 'Accessing data from ICs'
-    settings = ICobj.settings
-    # filenames
-    snapshotName = settings.filenames.snapshotName
-        
-    # particle positions
-    r = ICobj.pos.r
-    xyz = ICobj.pos.xyz
-    # Number of particles
-    nParticles = ICobj.pos.nParticles
-    # molecular mass
-    m = settings.physical.m
-    # star mass
-    m_star = settings.physical.M.copy()
-    # disk mass
-    m_disk = ICobj.sigma.m_disk.copy()
-    m_disk = match_units(m_disk, m_star)[0]
-    # mass of the gas particles
-    m_particles = m_disk / float(nParticles)
-    # re-scale the particles (allows making of lo-mass disk)
-    m_particles *= settings.snapshot.mScale
-    
-    # -------------------------------------------------
-    # Assign output
-    # -------------------------------------------------
-    print 'Assigning data to snapshot'
-    # Get units all set up
-    m_unit = m_star.units
-    pos_unit = r.units
-    
-    if xyz.units != r.units:
-        
-        xyz.convert_units(pos_unit)
-        
-    # time units are sqrt(L^3/GM)
-    t_unit = np.sqrt((pos_unit**3)*np.power((G*m_unit), -1)).units
-    # velocity units are L/t
-    v_unit = (pos_unit/t_unit).ratio('km s**-1')
-    # Make it a unit
-    v_unit = pynbody.units.Unit('{0} km s**-1'.format(v_unit))
-    
-    # Other settings
-    metals = settings.snapshot.metals
-    star_metals = metals
-    
-    # -------------------------------------------------
+    print 'Generating snapshot...'   
     # Initialize snapshot
-    # -------------------------------------------------
-    # Note that empty pos, vel, and mass arrays are created in the snapshot
-    snapshot = pynbody.new(star=1,gas=nParticles)
-    snapshot['vel'].units = v_unit
-    snapshot['eps'] = 0.01*SimArray(np.ones(nParticles+1, dtype=np.float32), pos_unit)
-    snapshot['metals'] = SimArray(np.zeros(nParticles+1, dtype=np.float32))
-    snapshot['rho'] = SimArray(np.zeros(nParticles+1, dtype=np.float32))
-    
-    snapshot.gas['pos'] = xyz
-    snapshot.gas['temp'] = ICobj.T(r)
-    snapshot.gas['mass'] = m_particles
-    snapshot.gas['metals'] = metals
-    
-    snapshot.star['pos'] = SimArray([[ 0.,  0.,  0.]],pos_unit)
-    snapshot.star['vel'] = SimArray([[ 0.,  0.,  0.]], v_unit)
-    snapshot.star['mass'] = m_star
-    snapshot.star['metals'] = SimArray(star_metals)
-    # Estimate the star's softening length as the closest particle distance
-    snapshot.star['eps'] = r.min()
-    
+    snapshot = init_snapshot(IC)
     # Make param file
+    param = init_param(IC, snapshot)
+       
+    # -------------------------------------------------
+    # CALCULATE VELOCITY USING calc_velocity.py.  This also estimates the 
+    # gravitational softening length eps and a good timestep
+    # -------------------------------------------------
+    print 'Calculating circular velocity'
+    preset = IC.settings.changa_run.preset
+    max_particles = global_settings['misc']['max_particles']
+    dDelta = calc_velocity.v_xy(snapshot, param, \
+    changa_preset=preset, max_particles=max_particles)
+    param['dDelta'] = dDelta    
+    print 'Calculated time step.  dDelta = ', dDelta
+    gc.collect()
+    
+    # Create director file
+    director = init_director(IC, param)
+    # Finalize
+    print 'Wrapping up'
+    setup_sinks(snapshot, param, r_sink = IC.pos.r.min())
+    
+    return snapshot, param, director
+
+def init_director(IC, param=None):
+    """
+    Creates a director dict for a PPD.
+    
+    Parameters
+    ----------
+    IC : ICobj
+    param : .param dict
+        If not supplied, param = IC.param
+    """    
+    if param is None:
+        
+        param = IC.snapshot_param
+        
+    # largest radius to plot
+    r_director = float(0.9 * IC.pos.r.max())
+    # Maximum surface density
+    sigma_min = float(IC.sigma(r_director))
+    # surface density at largest radius
+    sigma_max = float(IC.sigma.input_dict['sigma'].max())
+    # Create director dict
+    director = make_director(sigma_min, sigma_max, r_director, \
+    filename=param['achOutName'])
+    
+    return director
+    
+def setup_sinks(snapshot, param, r_sink=None):
+    """
+    Sets up snapshot and param for stars that are sinks.
+    
+    Parameters
+    ----------
+    snapshot : SimSnap
+    param : param dict
+    r_sink : SimArray-like
+        If provide, this will be the sink radius.  Otherwise it's taken to be
+        the minimum gas cylindrical radius
+    
+    Returns
+    -------
+    None
+    """
+    units = diskpy.pychanga.units_from_param(param)
+    
+    if r_sink is None:
+        
+        r_sink = snapshot.g['rxy'].min()
+        r_sink.convert_units(units['l_unit'])
+        
+    r_sink = strip_units(r_sink)
+    # Set the star tforms to a negative number.  This allows UW ChaNGa treat 
+    # stars as sink particles
+    snapshot.star['tform'] = -1.0    
+    # Update params
+    Mstar = snapshot.s['mass'].min()
+    Mstar.convert_units(units['m_unit'])
+    param['dSinkBoundOrbitRadius'] = r_sink
+    param['dSinkRadius'] = r_sink
+    param['dSinkMassMin'] = 0.9 * strip_units(Mstar)
+    param['bDoSinks'] = 1
+
+def init_param(IC, snapshot=None):
+    """
+    Initializes a ChaNGa param dict (see also diskpy.pychanga.make_param) for
+    an IC object.
+    
+    Parameters
+    ----------
+    IC : ICobject
+        Initial conditions object containing required settings for generating
+        the param dict
+    snapshot : SimSnap
+        Snapshot to create param for.  If None, IC.snapshot is used
+        
+    Returns
+    -------
+    param : dict
+        Dict containing the param.  Can be saved with diskpy.utils.configsave
+    """
+    if snapshot is None:
+        
+        snapshot = IC.snapshot
+        
+    # Make param file
+    settings = IC.settings
+    snapshotName = settings.filenames.snapshotName
     param = make_param(snapshot, snapshotName)
-    param['dMeanMolWeight'] = m
+    param['dMeanMolWeight'] = settings.physical.m
     eos = (settings.physical.eos).lower()
     
     if eos == 'adiabatic':
@@ -112,49 +165,56 @@ def snapshot_gen(ICobj):
         param['bGasAdiabatic'] = 1
         param['bGasIsothermal'] = 0
         
-    param['dConstGamma']
-       
-    gc.collect()
+    return param
+
+def init_snapshot(IC):
+    """
+    Initialize a snapshot for the IC object.  Requires that positions have
+    been created.
     
-    # -------------------------------------------------
-    # CALCULATE VELOCITY USING calc_velocity.py.  This also estimates the 
-    # gravitational softening length eps and a good timestep
-    # -------------------------------------------------
-    print 'Calculating circular velocity'
-    preset = settings.changa_run.preset
-    max_particles = global_settings['misc']['max_particles']
-    dDelta = calc_velocity.v_xy(snapshot, param, changa_preset=preset, max_particles=max_particles)
-    param['dDelta'] = dDelta    
-    print 'Calculated time step.  dDelta = ', dDelta
-    gc.collect()
+    Parameters
+    ----------
+    IC : ICobj
     
-    # -------------------------------------------------
-    # Create director file
-    # -------------------------------------------------
-    # largest radius to plot
-    r_director = float(0.9 * r.max())
-    # Maximum surface density
-    sigma_min = float(ICobj.sigma(r_director))
-    # surface density at largest radius
-    sigma_max = float(ICobj.sigma.input_dict['sigma'].max())
-    # Create director dict
-    director = make_director(sigma_min, sigma_max, r_director, filename=param['achOutName'])
-    ## Save .director file
-    #configsave(director, directorName, 'director')
+    Returns
+    -------
+    snapshot : SimSnap
+    """
+    # Get required settings from IC
+    settings = IC.settings
+    # particle positions
+    r = IC.pos.r
+    xyz = IC.pos.xyz
+    nParticles = IC.pos.nParticles
+    m_star = settings.physical.M
+    m_disk = IC.sigma.m_disk
+    m_disk = match_units(m_disk, m_star)[0]
+    m_particles = m_disk / float(nParticles)
+    metals = settings.snapshot.metals
+    # re-scale the particles (allows making of lo-mass disk)
+    m_particles *= settings.snapshot.mScale
     
-    # -------------------------------------------------
-    # Wrap up
-    # -------------------------------------------------
-    print 'Wrapping up'
-    # Now set the star particle's tform to a negative number.  This allows
-    # UW ChaNGa treat it as a sink particle.
-    snapshot.star['tform'] = -1.0
+    # Handle units
+    units = setup_units(m_star, r)
     
-    # Update params
-    r_sink = strip_units(r.min())
-    param['dSinkBoundOrbitRadius'] = r_sink
-    param['dSinkRadius'] = r_sink
-    param['dSinkMassMin'] = 0.9 * strip_units(m_star)
-    param['bDoSinks'] = 1
+    if xyz.units != r.units:
+        
+        xyz.convert_units(units['x'])
     
-    return snapshot, param, director
+    # Initialize arrays
+    snapshot = pynbody.new(star=1,gas=nParticles)
+    snapshot['vel'].units = units['v']
+    snapshot['eps'] = SimArray(0.01, units['x'])
+    snapshot['rho'] = 0.
+    snapshot['metals'] = metals
+    # Assign array values
+    snapshot.gas['pos'] = xyz
+    snapshot.gas['temp'] = IC.T(r)
+    snapshot.gas['mass'] = m_particles
+    
+    snapshot.star['pos'] = 0.
+    snapshot.star['mass'] = m_star
+    # Estimate the star's softening length as the closest particle distance
+    snapshot.star['eps'] = r.min()
+    
+    return snapshot
