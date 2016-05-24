dcfea2240e5b18e4b607f2f2f1763bf1c91c311e
Isaac Backus <ibackus@gmail.com>
2016-05-24 16:00:49 -0700

    Added cdf() to estimated cumulative distribution function from a pdf

--- diskpy/pdmath/__init__.py
+++ diskpy/pdmath/__init__.py
@@ -6,8 +6,8 @@ Created on Thu Jul 16 14:25:56 2015
 """
 
 from _math import extrap1d, meshinterp, smoothstep, digitize_threshold, \
-binned_mean, kepler_pos, resolvedbins, dA, setupbins, bin2dsum, bin2d
+binned_mean, kepler_pos, resolvedbins, dA, setupbins, bin2dsum, bin2d, cdf
 
 __all__ = ['extrap1d', 'meshinterp', 'smoothstep', 'digitize_threshold', 
 'binned_mean', 'kepler_pos', 'resolvedbins', 'dA', 'setupbins', 'bin2dsum', 
-'bin2d']
+'bin2d', 'cdf']
